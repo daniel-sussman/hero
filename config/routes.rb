28ed06a321cb76338c04572dddf8033fb6403988
Daniel Sussman <daniel.o.sussman@gmail.com>
2023-11-28 16:14:39 +0000

    root path

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :collections
 
   devise_for :users
-  root to: "/"
+  root "activities#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
