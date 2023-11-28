9bd162076c675061ee24c115e08ad892a024582e
Daniel Sussman <daniel.o.sussman@gmail.com>
2023-11-28 15:36:24 +0000

    Changed root url

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get 'activities/index'
   get 'activities/show'
   devise_for :users
-  root to: "pages#home"
+  root to: "/"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
