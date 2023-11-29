c3eb3775c27b543cf81685a98d6cfbd70b197882
Suhoan Kim <londonbusan@gmail.com>
2023-11-29 11:05:27 +0000

    category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,15 @@
 class CategoriesController < ApplicationController
+  skip_before_action :authentification [:index]
   def index
+    @categories = Category.all
   end
 
   def show
+    @category = Category.find(params[:id])
+    @activity = Activity.find(params[:id])
+    @activities = Activity.joins(:activity_categories)
+                  .joins(:categories)
+                  .where(“activity_categories.category_id = categories.id”)
+                  .select(“activities.name”)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,14 @@
 Rails.application.routes.draw do
-  get 'collections/index'
-  get 'collections/show'
-  get 'collections/new'
-  get 'collections/create'
-  get 'collections/edit'
-  get 'collections/update'
-  get 'collections/destroy'
-  get 'encounters/update'
-  get 'categories/index'
-  get 'categories/show'
-  get 'activities/index'
-  get 'activities/show'
+  resources :activities, only: [:index, :show]
+  resources :categories, only: [:index, :show]
+  resources :encounters, only: [:update]
+  resources :collections
   devise_for :users
-  root to: "pages#home"
+  root "activities#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
   # root "posts#index"
 end
