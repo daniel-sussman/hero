45e3ae035f8f6c4d556accc0eb70ebe188f2eba1
Daniel Sussman <daniel.o.sussman@gmail.com>
2023-11-29 17:42:13 +0000

    index page and _category_carousel

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,10 +2,15 @@ class ActivitiesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
-    @activities = Activity.all
+    @categories = Category.take(5)
+    @activities_first_category = Activity.take(5)
+    @activities_second_category = Activity.take(5)
+    @activities_third_category = Activity.take(5)
+    @activities_fourth_category = Activity.take(5)
+    @activities_fifth_category = Activity.take(5)
   end
 
   def show
-    @game = Activity(params[:id])
+    @activity = Activity.find(params[:id])
   end
 end
