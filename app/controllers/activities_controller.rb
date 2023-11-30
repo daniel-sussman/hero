8fc01f8aed44b61a5acdcd506e3bf5d6d7762e61
manojvasava <manojvasava5uk@gmail.com>
2023-11-30 15:35:13 +0000

    working on slider design

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,11 +4,10 @@ class ActivitiesController < ApplicationController
   def index
     @categories = Category.take(5)
     @all_recommended_activities = Activity.geocoded.take(7)
-    @activities_first_category = Activity.take(5)
-    @activities_second_category = Activity.take(5)
-    @activities_third_category = Activity.take(5)
-    @activities_fourth_category = Activity.take(5)
-    @activities_fifth_category = Activity.take(5)
+    @activities = []
+    @categories.each do |cat|
+      @activities << Activity.all.sample(5)
+    end
 
     #geocoding
     @coords = user_signed_in? ? [current_user.latitude, current_user.longitude] : [51.508045, -0.128217] #replace default coords with IP address coords
