48e1b89a9b904de512f8b090ca9ec5916e806d0a
Daniel Sussman <daniel.o.sussman@gmail.com>
2023-12-03 00:35:36 +0000

    Styled hearts, added map home icon

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -18,7 +18,7 @@ class ActivitiesController < ApplicationController
     end
 
     #geocoding
-    @coords = user_signed_in? ? [current_user.latitude, current_user.longitude] : [51.508045, -0.128217] #replace default coords with IP address coords
+    @coords = [current_user.latitude, current_user.longitude] if user_signed_in? #replace default coords with IP address coords
 
     @markers = @all_recommended_activities.map do |activity|
       {
@@ -28,12 +28,7 @@ class ActivitiesController < ApplicationController
         marker_html: render_to_string(partial: "marker_#{color_code(activity)}")
       }
     end
-
-    #search_function
-    @search_activities = Activity.all
-    if params[:query].present?
-      @search_activities = Activity.where("title ILIKE ?", "%#{params[:query]}%")
-    end
+    @home_marker = render_to_string(partial: "marker_home")
   end
 
   def show
